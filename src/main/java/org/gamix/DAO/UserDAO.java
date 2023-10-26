642b31356eade48c009cee8fb801d1fb41498bb0
gabrielOliv1 <89107513+gabrielOliv1@users.noreply.github.com>
2023-10-26 20:08:14 -0300

    Fix: fixing the update user method

--- src/main/java/org/gamix/DAO/UserDAO.java
+++ src/main/java/org/gamix/DAO/UserDAO.java
@@ -2,84 +2,107 @@ package org.gamix.DAO;
 
 import org.gamix.models.User;
 
-import java.util.Objects;
-
 public class UserDAO {
 
-    private final User[] vector = new User[10];
+    private User vector[] = new User[10];
+    private int userCount = 0;
+    private int idCount = 1;
 
-    // findAll
-
-    public void findAllUsers() {
-
-        for (User user : vector) {
-            System.out.println(
-                    "Username:" + user.getUsername() + "\n" +
-                            "Email:" + user.getEmail() + "\n" +
-                            "Icon: " + user.getIcon() + "\n" +
-                            "password: " + user.getPasswordUser().getPassword() + "\n"
-            );
+    public void insertUser(User user) {
+        if (userCount < vector.length) {
+            user.setId(idCount++);
+            vector[userCount++] = user;
+        } else {
+            System.out.println("Sua lista está cheia");
         }
     }
 
-    // findByEmail
+    public User login(String usernameOrEmail, String password) {
+        for (User user : vector) {
+            if (user != null && (user.getUsername().equals(usernameOrEmail) || user.getEmail().equals(usernameOrEmail)) &&
+                user.getPasswordUser().getPassword().equals(password)) {
+                return user;
+            }
+        }
+        return null; 
+    }
+
+    public void findAllUsers() {
+        for (User user : vector) {
+            if (user != null) {
+            	System.out.println("\n" + "--------------------------------------");
+                System.out.println("ID: " + user.getId());
+                System.out.println("Username: " + user.getUsername());
+                System.out.println("Email: " + user.getEmail());
+                System.out.println("Icon: " + user.getIcon());
+                System.out.println("Password: " + user.getPasswordUser().getPassword());
+                System.out.println("--------------------------------------");
+            }
+        }
+    }
 
     public void findByEmail(String email) {
         for (User user : vector) {
-            if (user.getEmail().equals(email)) {
-                System.out.println(
-                        "Username:" + user.getUsername() + "\n" +
-                                "Email:" + user.getEmail() + "\n" +
-                                "Icon: " + user.getIcon() + "\n" +
-                                "password: " + user.getPasswordUser().getPassword() + "\n"
-                );
-            }
+            if (user != null && user.getEmail().equals(email)) {
+            	System.out.println("\n" + "--------------------------------------");
+                System.out.println("ID: " + user.getId());
+                System.out.println("Username: " + user.getUsername());
+                System.out.println("Email: " + user.getEmail());
+                System.out.println("Icon: " + user.getIcon());
+                System.out.println("Password: " + user.getPasswordUser().getPassword());
+                System.out.println("--------------------------------------");
+            } 
         }
     }
 
     public void findByUsername(String username) {
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i].getUsername().equals(username)) {
-                System.out.println(
-                        "Username:" + vector[i].getUsername() + "\n" +
-                                "Email:" + vector[i].getEmail() + "\n" +
-                                "Icon: " + vector[i].getIcon() + "\n" +
-                                "password: " + vector[i].getPasswordUser().getPassword() + "\n"
-                );
-
-                break;
+        for (User user : vector) {
+            if (user != null && user.getUsername().equals(username)) {
+            	System.out.println("\n" + "--------------------------------------");
+                System.out.println("ID: " + user.getId());
+                System.out.println("Username: " + user.getUsername());
+                System.out.println("Email: " + user.getEmail());
+                System.out.println("Icon: " + user.getIcon());
+                System.out.println("Password: " + user.getPasswordUser().getPassword());
+                System.out.println("--------------------------------------");
             }
         }
     }
-    // update user
 
     public void updateUser(Integer id, User partialUserInput) {
+    	boolean exist = false;
         for (User user : vector) {
-            if (user.getId().equals(id)) {
+            if (user != null && user.getId().equals(id)) {
                 user.partialUserInput(partialUserInput);
-
+                exist = true;
                 break;
             }
+        }
+        if (exist == false) {
+        	System.out.println("Não existe usuário com esse ID");
         }
     }
 
     public void deleteAccount(Integer id) {
-        int indexToExclude = -1;
-
-        for (int i = 0; i < vector.length; i++) {
-            if (vector[i] != null && Objects.equals(vector[i].getId(), id)) {
-                indexToExclude = i;
+    	boolean exist = false;
+        for (int i = 0; i < userCount; i++) {
+            if (vector[i] != null && vector[i].getId().equals(id)) {
+                vector[i] = null;
+                exist = true;
+                System.out.println("Conta excluída com sucesso!");
+                userCount--;
+          
+                for (int j = i; j < userCount; j++) {
+                    vector[j] = vector[j + 1];
+                }
+                vector[userCount] = null; 
                 break;
             }
         }
-
-        if (indexToExclude >= 0) {
-            for (int i = indexToExclude; i < vector.length; i++) {
-                vector[i] = vector[i + 1];
-            }
-
-            vector[vector.length - 1] = null;
+        if (exist == false) {
+        	System.out.println("Não existe usuário com esse ID");
         }
     }
 
+    
 }
